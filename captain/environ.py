36c55a35a061e36428026623089b2d9b586c3104
Jay Marcyes <jay@marcyes.com>
2018-04-23 09:14:49 -0600

    updates comment

--- captain/environ.py
+++ captain/environ.py
@@ -4,6 +4,9 @@ import os
 
 
 QUIET_DEFAULT = os.environ.get("CAPTAIN_QUIET_DEFAULT", "D")
-"""By default, it's just better to suppress debug logging, but you can manipulate
-this value to whatever you want"""
+"""By default, this suppresses debug but you can manipulate this value to whatever
+you want, this should be a string milar to what --quiet takes, the reason we suppress
+DEBUG by default is because it will turn debug on for every logger in a default 
+captain run (meaning you haven't configured any loggers separately) and so having
+debug on by default can result in a lot of extraneous logging from installed modules"""
 
